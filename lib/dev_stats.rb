a0296790b8e549d73da82bae1d23aff92a99ae24
Pete Hawkins <petey@hey.com>
2021-10-22 17:44:58 +0200

    Get it working with entered API keys

--- lib/dev_stats.rb
+++ lib/dev_stats.rb
@@ -1,20 +1,45 @@
 # frozen_string_literal: true
 
-require "dotenv"
+require "pstore"
+require "readline"
 require_relative "dev_stats/version"
 require_relative "dev_stats/stats"
 require_relative "dev_stats/fetch_stats"
 require_relative "dev_stats/display"
-
-Dotenv.load
 
 module DevStats
   class Error < StandardError; end
 
   class CLI
     def run
-      stats = FetchStats.new(api_key: ENV.fetch("DEV_API_KEY")).latest_stats
-      STDOUT.puts Display.new.render(stats)
+      request_api_key unless api_key
+
+      stats = FetchStats.new(api_key: api_key).latest_stats
+      puts Display.new.render(stats)
+    end
+
+    def api_key
+      @_api_key = store.transaction(true) do |store|
+        store["api_key"]
+      end
+    end
+
+    def request_api_key
+      puts "What is your dev.to API key?"
+      @_api_key = api_key = Readline.readline("> ", true)
+
+      store.transaction do |store|
+        store["api_key"] = api_key
+      end
+
+      # Clear this output
+      print "\r" + ("\e[A\e[K"*2)
+    end
+
+    private
+
+    def store
+      PStore.new(File.expand_path("~/devstats.pstore"))
     end
   end
 end
