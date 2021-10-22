cd9cbe7e06101d8bb18e4775c115bb424c876360
Pete Hawkins <petey@hey.com>
2021-10-22 17:29:02 +0200

    Create bin file to run devstats

--- lib/dev_stats.rb
+++ lib/dev_stats.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
+require "dotenv"
 require_relative "dev_stats/version"
 require_relative "dev_stats/stats"
 require_relative "dev_stats/fetch_stats"
 require_relative "dev_stats/display"
 
+Dotenv.load
+
 module DevStats
   class Error < StandardError; end
-  # Your code goes here...
+
+  class CLI
+    def run
+      stats = FetchStats.new(api_key: ENV.fetch("DEV_API_KEY")).latest_stats
+      STDOUT.puts Display.new.render(stats)
+    end
+  end
 end
